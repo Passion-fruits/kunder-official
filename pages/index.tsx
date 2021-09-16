7e0066e2bc9c8a0a58758574f3a7556b8a55db42
jiwon <jidole041214@naver.com>
2021-09-16 20:28:00 +0900

    UPDATE

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,3 @@
-import MusicCard from "../components/MusicCard";
-
 export default function Home() {
-  return (
-    <MusicCard
-      title="aaa"
-      song_id="sdf"
-      cover_url="asdfjk"
-      like={1}
-      artist="asdf"
-    />
-  );
+  return <></>;
 }
