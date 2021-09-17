b6e5e87d07eb9ac145c30745e9e3872228222d11
jiwon <jidole041214@naver.com>
2021-09-17 15:08:49 +0900

    ADD circle arrow 추가

---
+++ assets/circleArrow.tsx
@@ -0,0 +1,30 @@
+export default function CircleArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="26.25"
+      height="26.25"
+      viewBox="0 0 29.25 29.25"
+    >
+      <g
+        id="Icon_ionic-ios-arrow-dropright"
+        data-name="Icon ionic-ios-arrow-dropright"
+        transform="translate(-3.375 -3.375)"
+      >
+        <path
+          id="패스_28"
+          data-name="패스 28"
+          d="M14.714,10.279a1.021,1.021,0,0,1,1.439,0l5.027,5.043a1.016,1.016,0,0,1,.032,1.4l-4.953,4.969a1.015,1.015,0,1,1-1.439-1.433l4.21-4.273-4.316-4.273A1,1,0,0,1,14.714,10.279Z"
+          transform="translate(1 2)"
+          fill="#5E5E5E"
+        />
+        <path
+          id="패스_29"
+          data-name="패스 29"
+          d="M3.375,18A14.625,14.625,0,1,0,18,3.375,14.623,14.623,0,0,0,3.375,18Zm2.25,0A12.37,12.37,0,0,1,26.747,9.253,12.37,12.37,0,1,1,9.253,26.747,12.269,12.269,0,0,1,5.625,18Z"
+          fill="#5E5E5E"
+        />
+      </g>
+    </svg>
+  );
+}
